3acc0e2ade73f814d9669c0a9aaff9ca6767c91a
carolkes <carolkestering@hotmail.com>
2019-10-23 15:36:37 -0300

    commit js e html - carol

--- AtividadeCorreios/AtividadeCorreios/Scripts/Utils/script.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/script.js
@@ -1,32 +1,48 @@
-﻿$(document).ready(function () {
+﻿var identificador = 0;
+
+$(document).ready(function () {
     $('#Calcular').click(function () {
-        var url = `http://usysweb.com.br/api/correiosambev.php?nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=70002900&sCepDestino=04547000&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=20&nVlLargura=20&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3`;
+
+        var cepOrigem = $('#ceporigem').val();
+
+        var url = `http://usysweb.com.br/api/correiosambev.php?nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=${cepOrigem}&sCepDestino=04547000&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=20&nVlLargura=20&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3`;
         $.get(url, function (data) {
-            var latim = JSON.parse(data);
-            $.each(latim.cServico, function (key, value) {
+            var dataJSON = JSON.parse(data);
+            console.log(dataJSON.cServico)
                 
 
-                var text = `<div class="accordion" id="accordion_div${key}">
-        <div class="card">
-            <div class="card-header" id="headingOne">
-                <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse_div${key}" aria-expanded="true" aria-controls="collapseOne">
-                        ${value.cServico}
-                    </button>
-                </h2>
-            </div>
-            </div>
-        </div>`
+                var text = `
+    <div class="card">
+        <div class="card-header" id="heading">
+            <h2 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${identificador}" aria-expanded="true" aria-controls="collapseOne">
+                    ${cepOrigem}
+        </button>
+            </h2>
+        </div>
 
-                $('#divContent').append(
-                    text
-                );
-         
+        <div id="collapse${identificador}" class="collapse show" aria-labelledby="heading" data-parent="#accordionExample">
+            <div class="card-body text-secondary">
+                Codigo: ${dataJSON.cServico.Codigo}
+                Valor: ${dataJSON.cServico.Valor}
+                Prazo de Entrega: ${dataJSON.cServico.PrazoEntrega}
+                Valor SemAdicionais: ${dataJSON.cServico.ValorSemAdicionais}
+            </div>
+        </div>
+    </div>`
 
-            }); 
+            $('#div_content').append(
+                text
+            );
+
+            identificador++;
+
         });
     });
 });
+
+
+
 
 //var text = `<p>
 //<button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#collapse_div${key}" aria-expanded="false" aria-controls="collapseExample">
