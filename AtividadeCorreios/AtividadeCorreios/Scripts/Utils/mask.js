a5e0013feeaa5622e2e2840fe34250138cb62d68
carolkes <carolkestering@hotmail.com>
2019-10-24 10:57:32 -0300

    adicionei mascaras e tentei linkar o botão calcular com outro index.html. mas não deu certo

---
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/mask.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#cepOrigem").mask("99999-999");
+    $("#cepDestino").mask("99999-999");
+    $("#peso").mask("#.##0,000", { reverse: true });
+    $("#comprimento").mask("#.##0,0", { reverse: true });
+    $("#altura").mask("#.##0,0", { reverse: true });
+    $("#largura").mask("#.##0,0", { reverse: true });
+    $("#diametro").mask("#.##0,00", { reverse: true });
+});
